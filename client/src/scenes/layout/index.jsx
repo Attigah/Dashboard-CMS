8e751653ae832a739b5d6167fa2c872329520a19
Attigah <bkeattigah@st.ug.edu.gh>
2024-07-04 02:28:22 +0000

    client and server setup-bg problem

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Box, use MediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+const Layout = () => {
+return <Box width="100%" height="100%">
+I
+</Box>;
+};
+export default Layout;
